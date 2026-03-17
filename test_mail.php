a4229b05515a0c9eda9f2af42afdf6f4bf03aef4
Ricard0MB <ricardomejias2808@gmail.com>
2026-03-17 11:17:08 -0300

    Actualizado Dockerfile con utilidades y agregado test_mail.php

---
+++ test_mail.php
@@ -0,0 +1,9 @@
+<?php
+require_once 'vendor/autoload.php';
+
+use PHPMailer\PHPMailer\PHPMailer;
+use PHPMailer\PHPMailer\Exception;
+
+$mail = new PHPMailer(true);
+echo "PHPMailer cargado correctamente.";
+?>
